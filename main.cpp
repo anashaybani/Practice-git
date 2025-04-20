6d3266cbfebce6c837ded4e91e7de56e0f070311
anashaybani <anashybani@gmail.com>
2025-04-20 17:27:14 +0330

    1commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+void printStudentID(){
+    int ID = 403613049;
+    cout << "Student ID: " <<ID << endl;
+}
+
+void printo() {
+    cout << "Hello, World!" << endl;
+}
+
+int main()
+{
+    printo();
+    printStudentID();
+
+    return 0;
+}
